332eb02bc6f10149c4bc588150cd0f7abf451279
samshax <samshax71@gmail.com>
2026-05-28 15:58:27 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,20 @@
 import google.generativeai as genai
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, filters, ContextTypes
-
 TELEGRAM_TOKEN = "8847168101:AAGVWvBSRExuyDNgenww6dQpy4q-dOKjgD4"
 GEMINI_KEY = "AIzaSyBBPYbUsSpL4rJ-Nj1k3BoteXW38Im78Yw"
-
 genai.configure(api_key=GEMINI_KEY)
 model = genai.GenerativeModel(
     model_name="gemini-2.0-flash",
     system_instruction="Sen yordamchi botsan. Foydalanuvchi qaysi tilda yozsa, shu tilda javob ber."
-)
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Salom! Savolingizni yozing 🤖")
-
 async def ai_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
     response = model.generate_content(user_message)
     await update.message.reply_text(response.text)
-
 app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, ai_reply))
 print("Bot ishga tushdi ✅")
 app.run_polling()
-
