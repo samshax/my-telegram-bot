05a2b7493685d7c4eee4193a1965bcef791873fc
samshax <samshax71@gmail.com>
2026-07-27 15:03:56 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,25 @@
-import google.generativeai as genai
+from groq import Groq
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, filters, ContextTypes
 
 TELEGRAM_TOKEN = "8847168101:AAGVWvBSRExuyDNgenww6dQpy4q-dOKjgD4"
-GEMINI_KEY = "AIzaSyCpP978SAWuZV3j2p5-FhtsTDpwc2enx9s"
+GROQ_KEY = "BU_YERGA_GROQ_KEYNI_YOZING"
 
-genai.configure(api_key=GEMINI_KEY)
-model = genai.GenerativeModel("gemini-2.0-flash")
+client = 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
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Salom! Savolingizni yozing 🤖")
 
 async def ai_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_message = update.message.text
-    response = model.generate_content(user_message)
-    await update.message.reply_text(response.text)
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {"role": "system", "content": "Sen yordamchi botsan. Foydalanuvchi qaysi tilda yozsa, shu tilda qisqa javob ber."},
+            {"role": "user", "content": user_message}
+        ]
+    )
+    await update.message.reply_text(response.choices[0].message.content)
 
 app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
